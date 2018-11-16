f65ace8de41af0871a05a01276ff139c9ca416c9
lilelr <lilelr@163.com>
2018-11-16 11:12:40 +0800

    forgego the system path and library of protobuf 2.6

--- cmake/protobuf_compile.cmake
+++ cmake/protobuf_compile.cmake
@@ -1,13 +1,17 @@
 # In this case, this function would:
 #
 #   (1)  given the drectory path where the proto file is, the name of proto file, and the output directory path of the relevant header and cpp, we will use protoc to
-#         to compile it and generate the header and cpp in the directory path. For example,
+#         to compile it and generate the header and cpp in the directory path.
+#         Finally, we use the generated header files and cpps to form a static library with ${OUT_PUT_LIBRARY} as the name.
+#         For example,
 #         PROTOC_COMPILE(${CMAKE_SOURCE_DIR}/src/protobuf_begin/proto SearchRequest ${CMAKE_BINARY_DIR}/src)
 #         PROTO_PATH: ${CMAKE_SOURCE_DIR}/src/protobuf_begin/proto
 #         PROTO_NAME: SearchRequest
 #         OUT_PUT_PATH: ${CMAKE_BINARY_DIR}/src
+#         OUT_PUT_LIBRARY: foo
 #         means that we will compile the ${CMAKE_SOURCE_DIR}/src/protobuf_begin/proto/SearchRequest.proto and generates
-#         SearchRequest.pb.h and SearchRequest.pb.cc under the directory ${CMAKE_BINARY_DIR}/src
+#         SearchRequest.pb.h and SearchRequest.pb.cc under the directory ${CMAKE_BINARY_DIR}/src.
+#         Finally, we could generate a static library libfoo.a including the SearchRequest.pb.h and SearchRequest.pb.cc.
 
 #   (2) export the following variables, based on the `PROTO_NAME` parameter
 #       (a) SearchRequest_PROTO:    ${CMAKE_SOURCE_DIR}/src/protobuf_begin/proto/SearchRequest
@@ -16,7 +20,7 @@
 
 
 message(18 ${CMAKE_SOURCE_DIR})
-function(PROTOC_COMPILE PROTO_PATH PROTO_NAME OUT_PUT_PATH)
+function(PROTOC_COMPILE PROTO_PATH PROTO_NAME OUT_PUT_PATH OUT_PUT_LIBRARY)
 
     set(TO_INCLUDE_DIR
             --cpp_out=${OUT_PUT_PATH}
@@ -46,7 +50,8 @@ function(PROTOC_COMPILE PROTO_PATH PROTO_NAME OUT_PUT_PATH)
             COMMAND ${protobuf_2_6_compiler} ${TO_INCLUDE_DIR} ${PROTO}
             )
     message("ends compile the .proto file ")
-    add_library(foo ${CC} ${H})
+    # expose the library with ${OUT_PUT_LIBRARY} as the name formed from the generated headers and cpps
+    add_library(${OUT_PUT_LIBRARY} ${CC} ${H})
 
 endfunction()
 
