89079519fdf30557f90c9f03fc4547cb889bf7bd
weiguow <2573777501@qq.com>
2018-11-22 16:37:55 +0800

    weiguo_cpuinfo

--- weiguo_server_client/student_slave.cpp
+++ weiguo_server_client/student_slave.cpp
@@ -13,8 +13,10 @@
 #include <process/process.hpp>
 #include <process/protobuf.hpp>
 
-#include <Student_pro.pb.h>
-#include <Transfile.pb.h>
+#include <student_pro.pb.h>
+#include <transfile.pb.h>
+#include <cpuinfo_pro.pb.h>
+
 
 using namespace process;
 
@@ -47,15 +49,63 @@ public:
         install("stop", [=](const UPID &from, const string &body) {
             terminate(self());
         });
-        /*
-         * 函数名：install
-         * 参数类型：<想要监听的message名字>
-         * 注册的方法名字，以及该方法用到message参数
-         * */
+
         install<Transfile>(&Student_Client::report_from_server, &Transfile::key);
+
+        install<AllCpuInfo_SingleInfo>(&Student_Client::report_cpuinfo_from_server_all, &AllCpuInfo_SingleInfo::cpuid,
+                                       &AllCpuInfo_SingleInfo::coreid, &AllCpuInfo_SingleInfo::physicalid,
+                                       &AllCpuInfo_SingleInfo::cpucores, &AllCpuInfo_SingleInfo::modelname,
+                                       &AllCpuInfo_SingleInfo::cpumhz, &AllCpuInfo_SingleInfo::l1dcache);
 
     }
 
+/************************************1.CPU information***************************************/
+
+    /**
+     * 2.to get all cpu information from master
+     * */
+    void send_request_cpuinfo_all() {
+        AllCpuInfo_SingleInfo as;
+        string client_id = this->self();
+        as.set_address(client_id);
+        send(server, as);
+    }
+
+    /**
+     * 3.get information from server
+     * */
+    void report_cpuinfo_from_server(const string &cpuid, const string &coreid){
+        cout << "cpuID:" << coreid << " coreID:" << coreid << endl;
+    }
+
+    /**
+     * 4.get all information from server
+     * */
+    void report_cpuinfo_from_server_all(const string &cpuid, const string &coreid, const string &physicalid,
+                                        const string &cpucores, const string &modelname, const string &cpumhz,
+                                        const string &l1dcache) {
+        cout << "CPU information" << endl;
+        cout << "cpuID:" << cpuid << "   coreID:" << coreid << "   Physical ID:" << physicalid
+             << "   cpu cores:" << cpucores << "   model name:" << modelname << "   cpu MHz:"
+             << cpumhz << "   L1d cache:" << l1dcache << endl;
+    }
+
+    /**
+     * 5.send for all information
+     * */
+     void send_server_a_key_cpu(){
+         AllCpuInfo ac;
+         string client_ID = this->self();
+         ac.set_key_cpu(client_ID);
+         send(server,ac);
+     }
+
+/***************************************************************************************/
+
+
+
+
+/*****************************2.send student message************************************/
     void report_from_server(const string &key) {
         cout << "report from server" << endl;
         cout << key << endl;
@@ -70,47 +120,44 @@ public:
         s.set_id(2);
         s.set_age(23);
 
-        cout << "name:" << s.name() <<" 学号：" << s.id() << " 年龄：" << s.age() << endl;
+        cout << "name:" << s.name() << " 学号：" << s.id() << " 年龄：" << s.age() << endl;
         send(server, s);
     }
 
     void send_server_a_file() {
         Transfile t;
         string client_id = this->self();
-        string str_file =  read_a_python_file();
+        string str_file = read_a_python_file();
         t.set_key(client_id);
         t.set_value(str_file);
         send(server, t);
     }
+/*****************************************************************************/
 
-    //从服务端传输一个python文件到客户端
-    /*ios::app：　　　以追加的方式打开文件
-　　   ios::ate：　　　文件打开后定位到文件尾，ios:app就包含有此属性
-　　   ios::binary：　以二进制方式打开文件，缺省的方式是文本方式。两种方式的区别见前文
-　　   ios::in：　　　 文件以输入方式打开(文件数据输入到内存)
-　　   ios::out：　　　文件以输出方式打开(内存数据输出到文件)
-　　   ios::nocreate： 不建立文件，所以文件不存在时打开失败
-　　   ios::noreplace：不覆盖文件，所以打开文件时如果文件存在失败
-　　   ios::trunc：　　如果文件存在，把文件长度设为0
-     */
+
+
+
+
+/*******************************3.send a file ***********************************/
     string read_a_python_file() {
         string str_f;
-        ifstream myfile("/home/weiguow/hello.py",ios_base::in);
+        ifstream myfile("/home/weiguow/hello.py", ios_base::in);
         if (myfile.fail()) {
             cout << "读取文件失败" << endl;
         } else {
             char ch;
-            while(myfile.get(ch)){
+            while (myfile.get(ch)) {
                 str_f += ch;
             }
-            if(myfile.eof()){
+            if (myfile.eof()) {
                 cout << "文件已经全部读取完成" << endl;
-                cout << "文件内容为:"<< str_f << endl;
+                cout << "文件内容为:" << str_f << endl;
                 myfile.close();
             }
         }
-        return  str_f;
+        return str_f;
     }
+/*********************************************************************/
 
 };
 
@@ -119,6 +166,7 @@ int main() {
 
     Student_Client client;
     PID<Student_Client> cur_client = process::spawn(client);
+
     cout << "Running client on " << process::address().ip << ":" << process::address().port << endl;  //显示ip地址和端口
     cout << "please input the master UPID:" << endl;
 
@@ -129,13 +177,15 @@ int main() {
     //调用UPID构造方法,输入参数mater_str
     UPID masterUPID(master_str);
     client.server = masterUPID;  //声明client的服务器是masterUPID
+
+
+    //向服务器发送消息请求
     client.send_server_a_message();  //client向server发送消息
-
     client.send_server_a_file();  //传输python文件
-
-//    client.read_a_python_file();
+    client.send_request_cpuinfo_all();
+    client.send_server_a_key_cpu();
     process::wait(client.self());
-//    client.report_from_server();
+
 
     return 0;
 }
--- weiguo_server_client/teacher_master.cpp
+++ weiguo_server_client/teacher_master.cpp
@@ -2,6 +2,8 @@
 // Created by weiguow on 18-10-31.
 //
 #include <iostream>
+#include <list>
+#include <string>
 
 #include <sstream>   //文件的输入与输出
 #include <fstream>
@@ -33,10 +35,12 @@
 #include <stout/os/write.hpp>
 #include <stout/tests/utils.hpp>
 
-#include <Student_pro.pb.h>
-#include <Transfile.pb.h>
+#include <student_pro.pb.h>
+#include <transfile.pb.h>
+#include <cpuinfo_pro.pb.h>
 
 #include <flags.h>
+#include <cpu_info.hpp>
 
 using namespace process;
 using namespace process::http;
@@ -51,6 +55,7 @@ using std::cerr;
 using std::cout;
 using std::endl;
 using std::map;
+using std::list;
 using std::string;
 using std::vector;
 using std::shared_ptr;
@@ -72,12 +77,15 @@ using flags::Warnings;
 
 using utils::arraySize;
 
+using weiguo_proc::CPU;
+
 class Teacher_Master : public ProtobufProcess<Teacher_Master> {
 
 public:
     Teacher_Master() : ProcessBase("Teacher_master") {
     }
 
+    UPID client;
 
     virtual void initialize() {
         //返回教师信息
@@ -138,18 +146,128 @@ public:
             terminate(self());
         });
 
-        //需要监听什么样的消息谁来监听
+        //想要监听什么消息就在本身定义一个含有想要监听到的参数的方法，方法名是自己来定义的
         install<Student_pro>(&Teacher_Master::report_from_client, &Student_pro::name);
 
-        install<Transfile>(&Teacher_Master::report_from_client_python_file,&Transfile::key,&Transfile::value);
+        install<Transfile>(&Teacher_Master::report_from_client_python_file, &Transfile::key, &Transfile::value);
 
+        install<AllCpuInfo_SingleInfo>(&Teacher_Master::report_all_cpuinfo_to_client,&AllCpuInfo_SingleInfo::address);
+
+        install<AllCpuInfo>(&Teacher_Master::report_all_cpuinfo_to_client_2,&AllCpuInfo::key_cpu);
+
+//        install<SingleCpuInfo>(&Teacher_Master::get_client_cpu_request, &SingleCpuInfo::cpu);   //test for single cpu information
     }
 
+/*****************1.report student message to client*************************/
     void report_from_client(const string &name) {
         cout << "你的学生是" << endl;
         cout << name << endl;
     }
+/****************************************************************************/
 
+
+
+/*****************2.report cpu information***************************************/
+//    void get_client_cpu_request(const string &address){
+//
+//        SingleCpuInfo sci;
+//        UPID clientUPID(address);
+//        Try<list<CPU>> cpus = weiguo_proc::cpus();
+//        auto cpuinfo = cpus.get().front();
+//
+//        string cpuID = cpuinfo.cpuID;
+//        string coreID = cpuinfo.coreID;
+//
+//        sci.set_cpu("1");
+//        sci.set_cpuid(cpuID);
+//        sci.set_coreid(coreID);
+//
+//        send(clientUPID,sci);
+//    }
+
+    void report_all_cpuinfo_to_client(const string &address){
+
+        cout << address << endl;
+        int j = 0;
+        AllCpuInfo_SingleInfo as;
+
+        UPID clientUPID(address);
+        Try<list<CPU>> cpus = weiguo_proc::cpus();
+        for(auto i = cpus.get().begin();i!=cpus.get().end();i++){
+            as.set_cpuid(i->cpuID);
+            as.set_coreid(i->coreID);
+            as.set_physicalid(i->physicalID);
+            as.set_cpucores(i->cpucores);
+            as.set_modelname(i->modelname);
+            as.set_cpumhz(i->cpuMHz);
+            as.set_l1dcache(i->L1dcache);
+            as.set_l1icache(i->L1icache);
+            as.set_l2cache(i->L2cache);
+            as.set_l3cache(i->L3cache);
+            send(clientUPID,as);
+            as.Clear();
+        }
+    }
+
+    void report_all_cpuinfo_to_client_2(const string &address){
+        AllCpuInfo ac;
+        UPID clientUPID(address);
+        Try<list<CPU>> cpus = weiguo_proc::cpus();
+        for(auto i = cpus.get().begin();i!=cpus.get().end();i++){
+            ac.add_sci()->set_cpuid(i->cpuID);
+            ac.add_sci()->set_coreid(i->coreID);
+            ac.add_sci()->set_physicalid((i->physicalID));
+            ac.add_sci()->set_cpucores(i->cpucores);
+            ac.add_sci()->set_modelname(i->modelname);
+            ac.add_sci()->set_cpumhz(i->cpuMHz);
+            ac.add_sci()->set_l1dcache(i->L1dcache);
+            ac.add_sci()->set_l1icache(i->L1icache);
+            ac.add_sci()->set_l2cache(i->L2cache);
+            ac.add_sci()->set_l3cache(i->L3cache);
+        }
+
+        for(auto i = ac.sci().begin();i!=ac.sci().end();i++){
+            cout << i->cpuid() << i->coreid() << i-> physicalid() << i->cpucores() << i->modelname()
+                 << i->modelname() << i->cpumhz() << i->l1dcache() << i->l1icache() << i->l2cache()
+                 << i->l3cache() << endl;
+        }
+
+        send(clientUPID,ac);
+    }
+
+/*********************************************************************************/
+
+
+
+/*******************************3.send a file ***********************************/
+    void report_from_client_python_file(const string &key, const string &value) {
+        cout << "entering into report" << endl;
+        StringImage(value, "./hello.py");
+        Try<ProcessTree> res = Fork(None(), Exec("python ./hello.py"))();
+
+        //接收到文件的同时返回信息给client
+        UPID clientUPID(key);
+        Transfile messa;
+
+        //传递的消息体要保持完整？Yes,否则会出现missing required fields 的问题 ？？ 是否把字段换成option就不会出现这种问题？？
+        messa.set_key("weiguow");
+        messa.set_value("server_value");
+        send(clientUPID, messa);
+    }
+
+    void StringImage(string binFile, const char *outImage) {
+        fstream imgFile(outImage, std::ios::binary | std::ios::out);
+        for (int i = 0; i < binFile.length(); ++i) {
+            imgFile << binFile[i];
+        }
+        imgFile.close();
+    }
+/*********************************************************************************/
+
+
+
+
+/******************************4.dispatch*********************************************/
 
     void dispatch1() {
         process::dispatch(self(), &Teacher_Master::dispatch2);
@@ -178,7 +296,10 @@ public:
         cout << stu.age() << endl;
         process::dispatch(self(), &Teacher_Master::dispatch4);
     }
+/********************************************************************************/
 
+
+/*****************************5.test flags***************************************/
     void TestFlags() {
         TestFlagsBase flags;
         const char *argv[] = {
@@ -202,43 +323,23 @@ public:
         cout << flags.usage() << endl;
     }
 
-    void report_from_client_python_file(const string &key,const string &value){
-        cout << "entering into report" << endl;
-        StringImage(value,"./hello.py");
-        Try<ProcessTree> res = Fork(None(),Exec("python ./hello.py"))();
+/*********************************************************************************/
 
-        //接收到文件的同时返回信息给client
-        UPID clientUPID(key);
-        Transfile messa;
-
-        //传递的消息体要保持完整？
-        messa.set_key("weiguow");
-        messa.set_value("server_value");
-        send(clientUPID,messa);
-    }
-
-    void StringImage(string binFile,const char* outImage)
-    {
-        fstream imgFile(outImage,std::ios::binary|std::ios::out);
-        for(int i=0; i<binFile.length(); ++i)
-        {
-            imgFile << binFile[i];
-        }
-        imgFile.close();
-    }
 };
 
 int main() {
     //Bind ip and port
 //    net::IP ip = process::address().ip;
-    os::setenv("LIBPROCESS_IP", strings::trim("172.20.110.100"));
+//    os::setenv("LIBPROCESS_IP", strings::trim("172.20.110.81"));
+
     os::setenv("LIBPROCESS_PORT", stringify(2573));
 
     process::initialize("master");  //初始化一个名为master的进程
     Teacher_Master master;  //创建类Master的对象master
     PID<Teacher_Master> cur_master = process::spawn(master);  //启动master
 
-    // dispatch的循环调度
+
+    //dispatch的循环调度
 //    master.dispatch1();
 //    master.dispatch4();
 
@@ -247,8 +348,8 @@ int main() {
 
     cout << "Running server on " << process::address().ip << ":" << process::address().port << endl;
     const PID<Teacher_Master> masterPid = master.self();
-    cout << masterPid << endl;
 
+    cout << masterPid << endl;
 
     process::wait(master.self());
     return 0;
