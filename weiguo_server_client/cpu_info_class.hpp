80f09cf70a37ed60d28796852760b078b947c16c
weiguow <2573777501@qq.com>
2018-11-29 20:13:45 +0800

    modify cpu struct to clss

--- weiguo_server_client/cpu_info_class.hpp
+++ weiguo_server_client/cpu_info_class.hpp
@@ -30,35 +30,76 @@ using std::vector;
 using std::string;
 using std::cout;
 using std::endl;
+using std::list;
 using std::istringstream;
+using std::ostream;
 
 using strings::tokenize;
 
 using process::subprocess;
 using process::Subprocess;
 
-namespace weiguo_proc {
 
-    struct CPU {
-        CPU(string _cpuID, string _coreID, string _physicalID, string _cpucores, string _modelname,
-            string _cpuMHz, string _L1dcache, string _L1icache, string _L2cache, string _L3cache)
-                : cpuID(_cpuID), coreID(_coreID), physicalID(_physicalID), cpucores(_cpucores), modelname(_modelname),
-                  cpuMHz(_cpuMHz), L1dcache(_L1dcache), L1icache(_L1icache), L2cache(_L2cache), L3cache(_L3cache) {}
+/**
+ * 定义struct CPU,数据结构
+ */
+class cpu_collector {
 
-        // These are non-const because we need the default assignment operator.
-        string cpuID; // "processor"
-        string coreID; // "core id"
-        string physicalID; // "physical id"
-        string cpucores;  // "cpu cores"
-        string modelname;  //"model name"
-        string cpuMHz;  //"cpu MHz"
-        string L1dcache;  //"L1dcache"
-        string L1icache;  //"L1icache"
-        string L2cache;  //"L2cache"
-        string L3cache;  //"L3cache"
-    };
+private:
+    string cpuID; // "processor"
+    string coreID; // "core id"
+    string physicalID; // "physical id"
+    string cpucores;  // "cpu cores"
+    string modelname;  //"model name"
+    string cpuMHz;  //"cpu MHz"
+    string L1dcache;  //"L1dcache"
+    string L1icache;  //"L1icache"
+    string L2cache;  //"L2cache"
+    string L3cache;  //"L3cache"
 
-    inline std::ostream &operator<<(std::ostream &stream, const CPU &cpu) {
+public:
+
+    const string &getCpuID() const {
+        return cpuID;
+    }
+    
+    const string &getCoreID() const {
+        return coreID;
+    }
+
+    const string &getPhysicalID() const {
+        return physicalID;
+    }
+
+    const string &getCpucores() const {
+        return cpucores;
+    }
+
+    const string &getModelname() const {
+        return modelname;
+    }
+
+    const string &getCpuMHz() const {
+        return cpuMHz;
+    }
+
+    const string &getL1dcache() const {
+        return L1dcache;
+    }
+
+    const string &getL1icache() const {
+        return L1icache;
+    }
+
+    const string &getL2cache() const {
+        return L2cache;
+    }
+
+    const string &getL3cache() const {
+        return L3cache;
+    }
+
+    friend ostream &operator<<(std::ostream &stream, cpu_collector &cpu) {
         return stream << "CPU (id:" << cpu.cpuID << ", "
                       << "core ID:" << cpu.coreID << ", "
                       << "physical ID:" << cpu.physicalID << ", "
@@ -71,17 +112,27 @@ namespace weiguo_proc {
                       << "L3 cache:" << cpu.L3cache << ")";
     }
 
-// Reads from /proc/cpuinfo and returns a list of CPUs.
-    inline Try<std::list<CPU>> cpus() {
+    cpu_collector(string _cpuID, string _coreID, string _physicalID, string _cpucores, string _modelname,
+                           string _cpuMHz, string _L1dcache, string _L1icache, string _L2cache, string _L3cache)
+            : cpuID(_cpuID), coreID(_coreID), physicalID(_physicalID), cpucores(_cpucores), modelname(_modelname),
+              cpuMHz(_cpuMHz), L1dcache(_L1dcache), L1icache(_L1icache), L2cache(_L2cache), L3cache(_L3cache) {}
+
+    // These are non-const because we need the default assignment operator.
+
+    /**
+     * Reads from /proc/cpuinfo and returns a list of CPU
+     */
+
+    list<cpu_collector> get_cpu_info() {
 
         //gain the information of cpu
-        std::list<CPU> results;
+        std::list<cpu_collector> results;
 
         std::ifstream file("/proc/cpuinfo");
 
 
         if (!file.is_open()) {
-            return Error("Failed to open /proc/cpuinfo");
+            cout << "Failed to open /proc/cpuinfo" << endl;
         }
 
         // Placeholders as we parse the file.
@@ -112,8 +163,7 @@ namespace weiguo_proc {
 
 
                 if (split.size() < 2) {
-                    return Error("Unexpected format in /proc/cpuinfo: " +
-                                 stringify(split));
+                    cout << "Unexpected format in /proc/cpuinfo: " << endl;
                 }
 
                 /*.get方法和isSome是Option封装的信息
@@ -122,32 +172,32 @@ namespace weiguo_proc {
                 if (line.find("processor") == 0) {
                     if (cpuID.isSome()) {  //char的默认值是false
                         // The physical id and core id are not present in this case.
-                        results.push_back(CPU(cpuID.get(), 0, 0, 0, 0, 0, 0, 0, 0, 0));
+                        results.push_back(cpu_collector(cpuID.get(), 0, 0, 0, 0, 0, 0, 0, 0, 0));
                     }
                     cpuID = split.back().data();
                 } else if (line.find("physical id") == 0) {
                     if (physicalID.isSome()) {
-                        return Error("Unexpected physical ID in /proc/cpuinfo");
+                        cout << "Unexpected physical ID in /proc/cpuinfo" << endl;
                     }
                     physicalID = split.back().data();
                 } else if (line.find("core id") == 0) {
                     if (coreID.isSome()) {
-                        return Error("Unexpected core ID in /proc/cpuinfo");
+                        cout << "Unexpected core ID in /proc/cpuinfo" << endl;
                     }
                     coreID = split.back().data();
                 } else if (line.find("cpu cores") == 0) {
                     if (cpucores.isSome()) {
-                        return Error("Unexpected cpu cores in /proc/cpuinfo");
+                        cout << "Unexpected cpu cores in /proc/cpuinfo" << endl;
                     }
                     cpucores = split.back().data();
                 } else if (line.find("model name") == 0) {
                     if (modelname.isSome()) {
-                        return Error("Unexpected model name in /proc/cpuinfo");
+                        cout << "Unexpected model name in /proc/cpuinfo" << endl;
                     }
                     modelname = split.back().data();
                 } else if (line.find("cpu MHz") == 0) {
                     if (cpuMHz.isSome()) {
-                        return Error("Unexpected cpu MHz in /proc/cpuinfo");
+                        cout << "Unexpected cpu MHz in /proc/cpuinfo" << endl;
                     }
                     cpuMHz = split.back().data();
                 }
@@ -161,7 +211,7 @@ namespace weiguo_proc {
                 process::Future<string> ganied = process::io::read(cache.get().out().get());
                 vector<string> lscpu = strings::split(ganied.get(), "\n");
                 for (int i = 0; i < lscpu.size(); i++) {
-                    vector<string> vec = strings::split(lscpu[i],":");
+                    vector<string> vec = strings::split(lscpu[i], ":");
                     if (vec.front() == "L1d cache") {
                         L1dcache = strings::trim(vec.back());
                     } else if (vec.front() == "L1i cache") {
@@ -175,9 +225,9 @@ namespace weiguo_proc {
 
                 //finally create a CPU if we have all the information.
                 if (cpuID.isSome() && coreID.isSome() && physicalID.isSome() && cpucores.isSome() &&
-                    modelname.isSome() && cpuMHz.isSome() && L1dcache.isSome() && L2cache.isSome() && L1dcache.isSome()
-                    && L3cache.isSome()) {
-                    results.push_back(CPU(cpuID.get(), coreID.get(), physicalID.get(), cpucores.get(), modelname.get(),
+                    modelname.isSome() && cpuMHz.isSome() && L1dcache.isSome() && L2cache.isSome() &&
+                    L1dcache.isSome() && L3cache.isSome()) {
+                    results.push_back(cpu_collector(cpuID.get(), coreID.get(), physicalID.get(), cpucores.get(), modelname.get(),
                                           cpuMHz.get(), L1dcache.get(), L1icache.get(), L2cache.get(), L3cache.get()));
                     cpuID = None();
                     coreID = None();
@@ -195,14 +245,15 @@ namespace weiguo_proc {
         // Add the last processor if the physical id and core id were not present.
         if (cpuID.isSome()) {
             // The physical id and core id are not present.
-            results.push_back(CPU(cpuID.get(), 0, 0, 0, 0, 0, 0, 0, 0, 0));
+            results.push_back(cpu_collector(cpuID.get(), 0, 0, 0, 0, 0, 0, 0, 0, 0));
         }
         if (file.fail() && !file.eof()) {
-            return Error("Failed to read /proc/cpuinfo");
+            cout << "Failed to read /proc/cpuinfo" << endl;
         }
         return results;
     }
-}
+};
+
 
 #endif //LIBPROCESS_START_CPU_INFO_H
 
