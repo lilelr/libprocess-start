895c7f66160d7c7c9af2fddef682160601b74f1e
lilelr <lilelr@163.com>
2019-02-03 15:37:04 +0800

    local socket

--- actor/actor.cpp
+++ actor/actor.cpp
@@ -7,24 +7,32 @@
 
 #include <errno.h>
 #include <limits.h>
+
 #ifndef __WINDOWS__
+
 #include <netdb.h>
+
 #endif // __WINDOWS__
+
 #include <signal.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #ifndef __WINDOWS__
+
 #include <unistd.h>
 
 #include <arpa/inet.h>
+
 #endif // __WINDOWS__
 
 #include <glog/logging.h>
 
 #ifndef __WINDOWS__
+
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 
@@ -35,6 +43,7 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+
 #endif // __WINDOWS__
 
 #include <algorithm>
@@ -55,29 +64,74 @@
 #include <utility>
 #include <vector>
 
+#include <process/address.hpp>
+#include <process/check.hpp>
+#include <process/clock.hpp>
+#include <process/defer.hpp>
+#include <process/delay.hpp>
+#include <process/dispatch.hpp>
+#include <process/executor.hpp>
+#include <process/filter.hpp>
+#include <process/future.hpp>
+#include <process/gc.hpp>
+#include <process/help.hpp>
+#include <process/id.hpp>
+#include <process/io.hpp>
+#include <process/logging.hpp>
+#include <process/mime.hpp>
+#include <process/owned.hpp>
+#include <process/profiler.hpp>
+#include <process/reap.hpp>
+#include <process/sequence.hpp>
+#include <process/socket.hpp>
+#include <process/statistics.hpp>
+#include <process/system.hpp>
+#include <process/time.hpp>
+#include <process/timer.hpp>
+
+using process::network::inet::Address;
+using process::network::inet::Socket;
+
+using process::network::internal::SocketImpl;
 //using process::wait; // Necessary on some OS's to disambiguate.
-namespace actor{
+namespace actor {
 
 
-void ActorBase::enqueue(MyEvent* event, bool inject){
-   CHECK(event!= nullptr);
+    static std::atomic_bool initialize_started(false);
+    static std::atomic_bool initialize_complete(false);
 
-    synchronized(mutex){
-        if(state!= TERMINATING && state!= TERMINATED){
-            if(!inject){
-                events.push_back(event);
-            }else{
-                events.push_front(event);
-            }
+    static const int LISTEN_BACKLOG = 5000;
 
-            if(state == BLOCKED){
-                state = READY;
-            }
+    static Socket* __s__ = nullptr;
 
-            CHECK(state==BOTTOM || state == READY || state == RUNNING);
-        }else{
-            delete event;
-        }
+    void ActorBase::ActorBase(const std::string &id) {
+//        actor::
     }
-}
+
+    void ActorBase::enqueue(MyEvent *event, bool inject) {
+        CHECK(event != nullptr);
+
+        synchronized(mutex)
+            {
+                if (state != TERMINATING && state != TERMINATED) {
+                    if (!inject) {
+                        events.push_back(event);
+                    } else {
+                        events.push_front(event);
+                    }
+
+                    if (state == BLOCKED) {
+                        state = READY;
+                    }
+
+                    CHECK(state == BOTTOM || state == READY || state == RUNNING);
+                } else {
+                    delete event;
+                }
+            }
+    }
+
+    bool initialize(const Option<std::string>& delegate = None()){
+
+    }
 }
--- actor/actor.hpp
+++ actor/actor.hpp
@@ -31,6 +31,13 @@ namespace actor {
 
     class ActorBase : public MyEventVisitor {
     public:
+
+        explicit ActorBase(const std::string& id="");
+
+        virtual ~ActorBase();
+
+        process::UPID self() const {return pid;}
+
         void enqueue(MyEvent* event, bool inject = false);
         template<typename T>
         size_t eventCount(){
@@ -41,15 +48,39 @@ namespace actor {
             return count;
         }
 
-//    protected:
+    protected:
+        // Invoked when an event is serviced.
+        virtual void serve(const MyEvent& event){
+            event.visit(this);
+        }
 //
-//        virtual void visit(const MyMessageEvent &);
-//
-//        virtual void visit(const MyDispatchEvent &);
-//
-//        virtual void visit(const MyExitedEvent &);
-//
-//        virtual void visit(const MyTerminateEvent &);
+    //   Callbacks used to bisit (i.e. handle) specific event
+        virtual void visit(const MyMessageEvent &);
+
+        virtual void visit(const MyDispatchEvent &);
+
+        virtual void visit(const MyExitedEvent &);
+
+        virtual void visit(const MyTerminateEvent &);
+
+        /**
+ * Invoked when a actor gets spawned.
+ */
+        virtual void initialize() {}
+
+        /**
+         * Invoked when a actor is terminated.
+         *
+         * **NOTE**: this does not get invoked automatically if
+         * `visit(const TerminateEvent&)` is overridden.
+         */
+        virtual void finalize() {}
+
+
+        void send(const process::UPID& to,
+                  const std::string& name,
+                  const char* data = nullptr,
+                    size_t length = 0);
 
 
     private:
@@ -77,5 +108,14 @@ namespace actor {
         std::atomic_long refs;
         process::UPID pid;
     };
+
+/**
+* Initialize the library.
+*
+* **NOTE**: `actor` uses Google's `glog` and you can specify options
+* for it (e.g., a logging directory) via environment variables.
+ * */
+
+    bool initialize(const Option<std::string>& delegate = None());
 }
 #endif //LIBPROCESS_START_ACTOR_HPP
